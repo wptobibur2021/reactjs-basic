354e1939f09811321dd44c183affb8586734c5cf
unknown <wptobibur2021>
2021-07-06 20:59:18 +0600

    JSX File Update

--- src/component/Concept-JSX.jsx
+++ src/component/Concept-JSX.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 class ConceptJsx extends Component {
     render() {
+        return React.createElement('div', {className:'App'}, [
+            React.createElement('h1', null, 'Welcome'),
+            React.createElement('p', null, 'This is JSX File basic concept')
+        ])
         return (
             <div className="App">
                 <h1>Welcome</h1>
